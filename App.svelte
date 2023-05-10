b40f5f5d84891ae8321a62ce82ba530b4cd65d22
ADD-William-WaltersDavis <william.waltersdavis@dft.gov.uk>
2023-05-10 16:11:26 +0100

    Reset password for deployment

--- App.svelte
+++ App.svelte
@@ -17,7 +17,7 @@
   export let innerWidth = 0;
   export let innerHeight = 0;
   export let login_username = "user";
-  let login_password = "connect";
+  let login_password = "password";
   let squareID;
   let tileOpacity;
   let hoverInfo;
